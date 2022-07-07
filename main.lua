5fa59512e47b0379bb35727b0ea19f5b7c5bf58b
worcestershire sauce <whakened@gmail.com>
2022-07-07 17:23:54 -0400

    display options pretty much fine

--- main.lua
+++ main.lua
@@ -27,12 +27,18 @@ function love.load()
     hoveringExit = false
     hoveringOptions = false
     hoveringBegin = false
-    hoverShades = {255,255,255}
+    hoveringDisplay = false
+    hoveringMouse = false
+    hoveringGame = false
+    hoveringBack = false
+    hoverShades = {255,255,255,255,255,255,255}
+    useFancyBackground=true
+    hoveringBrightness=false
+    brightness=0
 end
 
 function love.update(dt)
-    if scene=="title" then
-        nodes[nodesToGenerate+1]={love.mouse.getX(),love.mouse.getY(),5}
+    if scene~="game" then
         if rgbDirection==1 then
             if backgroundG==255 then
                 rgbDirection=2
@@ -70,12 +76,14 @@ function love.update(dt)
                 backgroundB=backgroundB-1
             end
         end
-        if love.mouse.getX()>=548 and love.mouse.getY()>=552 and love.mouse.getX()<=654 and love.mouse.getY()<=589 then
-            hoveringExit=true
-            if hoverShades[3]~=0 then hoverShades[3]=hoverShades[3]-15 end
+    end
+    if scene=="title" then
+        if love.mouse.getX()>=525 and love.mouse.getY()>=402 and love.mouse.getX()<=673 and love.mouse.getY()<=439 then
+            hoveringBegin=true
+            if hoverShades[1]~=0 then hoverShades[1]=hoverShades[1]-15 end
         else
-            hoveringExit=false
-            if hoverShades[3]~=255 then hoverShades[3]=hoverShades[3]+15 end
+            hoveringBegin=false
+            if hoverShades[1]~=255 then hoverShades[1]=hoverShades[1]+15 end
         end
         if love.mouse.getX()>=488 and love.mouse.getY()>=477 and love.mouse.getX()<=710 and love.mouse.getY()<=515 then
             hoveringOptions=true
@@ -84,18 +92,60 @@ function love.update(dt)
             hoveringOptions=false
             if hoverShades[2]~=255 then hoverShades[2]=hoverShades[2]+15 end
         end
-        if love.mouse.getX()>=525 and love.mouse.getY()>=402 and love.mouse.getX()<=673 and love.mouse.getY()<=439 then
-            hoveringBegin=true
-            if hoverShades[1]~=0 then hoverShades[1]=hoverShades[1]-15 end
+        if love.mouse.getX()>=548 and love.mouse.getY()>=552 and love.mouse.getX()<=654 and love.mouse.getY()<=589 then
+            hoveringExit=true
+            if hoverShades[3]~=0 then hoverShades[3]=hoverShades[3]-15 end
         else
-            hoveringBegin=false
-            if hoverShades[1]~=255 then hoverShades[1]=hoverShades[1]+15 end
+            hoveringExit=false
+            if hoverShades[3]~=255 then hoverShades[3]=hoverShades[3]+15 end
+        end
+    elseif string.sub(scene,1,7)=="options" then
+        if love.mouse.getX()>=201 and love.mouse.getY()>=52 and love.mouse.getX()<=360 and love.mouse.getY()<=99 then
+            hoveringDisplay=true
+            if hoverShades[4]~=0 then hoverShades[4]=hoverShades[4]-15 end
+        else
+            hoveringDisplay=false
+            if hoverShades[4]~=255 then hoverShades[4]=hoverShades[4]+15 end
+        end
+        if love.mouse.getX()>=527 and love.mouse.getY()>=52 and love.mouse.getX()<=672 and love.mouse.getY()<=89 then
+            hoveringMouse=true
+            if hoverShades[5]~=0 then hoverShades[5]=hoverShades[5]-15 end
+        else
+            hoveringMouse=false
+            if hoverShades[5]~=255 then hoverShades[5]=hoverShades[5]+15 end
+        end
+        if love.mouse.getX()>=847 and love.mouse.getY()>=52 and love.mouse.getX()<=1073 and love.mouse.getY()<=99 then
+            hoveringGameplay=true
+            if hoverShades[6]~=0 then hoverShades[6]=hoverShades[6]-15 end
+        else
+            hoveringGameplay=false
+            if hoverShades[6]~=255 then hoverShades[6]=hoverShades[6]+15 end
+        end
+        if love.mouse.getX()>=548 and love.mouse.getY()>=552 and love.mouse.getX()<=654 and love.mouse.getY()<=589 then
+            hoveringBack=true
+            if hoverShades[7]~=0 then hoverShades[7]=hoverShades[7]-15 end
+        else
+            hoveringExit=false
+            if hoverShades[7]~=255 then hoverShades[7]=hoverShades[7]+15 end
+        end
+        if scene=="options-display" then
+            if love.mouse.getX()>=327 and love.mouse.getY()>=203 and love.mouse.getX()<=874 and love.mouse.getY()<=250 then
+                hoveringFancyBackground=true
+            else
+                hoveringFancyBackground=false
+            end
+            if love.mouse.getX()>=427 and love.mouse.getY()>=277 and love.mouse.getX()<=773 and love.mouse.getY()<=325 then
+                hoveringBrightness=true
+            else
+                hoveringBrightness=false
+            end
         end
     end
 end
 
 function love.draw()
-    if scene=="title" then
+    if scene~="game" and useFancyBackground then
+        nodes[nodesToGenerate+1]={love.mouse.getX(),love.mouse.getY(),5}
         love.graphics.setColor(backgroundR/255,backgroundG/255,backgroundB/255)
         for _, i in ipairs(nodes) do --I CANT READ ANY OF THIS LMAO
             for _, o in ipairs(nodes) do
@@ -134,6 +184,8 @@ function love.draw()
             i[1]=i[1]+love.math.random(-1,1)/2
             i[2]=i[2]+love.math.random(-1,1)/2
         end
+    end
+    if scene=="title" then
         love.graphics.setColor(1, 1, 1)
         love.graphics.rectangle("fill", love.mouse.getX(), 0, 1, 650)
         love.graphics.rectangle("fill", 0, love.mouse.getY(), 1200, 1)
@@ -166,9 +218,89 @@ function love.draw()
             love.graphics.print(love.mouse.getX() ..", ".. love.mouse.getY(), love.mouse.getX(), love.mouse.getY())
         end
     end
+    if scene=="options-display" then
+        love.graphics.setFont(helvetica_button)
+        love.graphics.setColor(1,1,1,0.25)
+        love.graphics.rectangle("fill", 320, 190, 560, 65, 15, 15)
+        love.graphics.rectangle("fill", 420, 265, 360, 65, 15, 15)
+        if useFancyBackground then
+            love.graphics.setColor(0,1,0)
+        else
+            love.graphics.setColor(1,0,0)
+        end
+        love.graphics.printf("Fancy Background: ".. tostring(useFancyBackground), 0, 200, 1200, "center")
+        love.graphics.setColor(1,1,1)
+        love.graphics.printf("Brightness: ".. tostring(brightness*2), 0, 275, 1200, "center")
+    end
+    if string.sub(scene,1,7)=="options" then
+        love.graphics.setColor(1,1,1,0.25)
+        love.graphics.rectangle("fill", 80, 40, 1040, 70, 25, 25)
+        love.graphics.rectangle("fill", 525, 540, 150, 65, 15, 15)
+        love.graphics.setFont(helvetica_button)
+        love.graphics.setColor(1,hoverShades[4]/255,hoverShades[4]/255)
+        love.graphics.printf("Display",100,50,360,"center")
+        love.graphics.setColor(1,hoverShades[5]/255,hoverShades[5]/255)
+        love.graphics.printf("Mouse",0,50,1200,"center")
+        love.graphics.setColor(1,hoverShades[6]/255,hoverShades[6]/255)
+        love.graphics.printf("Gameplay",780,50,360,"center")
+        love.graphics.setColor(1,hoverShades[7]/255,hoverShades[7]/255)
+        love.graphics.printf("Back",0,550,1200,"center")
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.printf("   |  |   ", 0, 50, 1200, "justify")
+        --love.graphics.print("|",371,50)
+        --love.graphics.print("|",749,50)
+        love.graphics.rectangle("fill", love.mouse.getX(), 0, 1, 650)
+        love.graphics.rectangle("fill", 0, love.mouse.getY(), 1200, 1)
+        if drawSelectionBox then
+            love.graphics.setColor(0.5, 0.75, 1, 1)
+            love.graphics.rectangle("line", mouseOriginalX, mouseOriginalY, love.mouse.getX()-mouseOriginalX, love.mouse.getY()-mouseOriginalY)
+            love.graphics.setColor(0.5, 0.75, 1, 0.3)
+            love.graphics.rectangle("fill", mouseOriginalX, mouseOriginalY, love.mouse.getX()-mouseOriginalX, love.mouse.getY()-mouseOriginalY)
+            love.graphics.setColor(0, 0, 0, 1)
+            love.graphics.setNewFont(12)
+            love.graphics.print(mouseOriginalX ..", ".. mouseOriginalY, mouseOriginalX+1, mouseOriginalY+1)
+            love.graphics.print(love.mouse.getX() ..", ".. love.mouse.getY(), love.mouse.getX()+1, love.mouse.getY()+1)
+            love.graphics.setColor(1, 1, 1, 1)
+            love.graphics.print(mouseOriginalX ..", ".. mouseOriginalY, mouseOriginalX, mouseOriginalY)
+            love.graphics.print(love.mouse.getX() ..", ".. love.mouse.getY(), love.mouse.getX(), love.mouse.getY())
+        end
+    end
+    if brightness<0 then
+        love.graphics.setColor(0,0,0,math.abs(brightness/10))
+    else
+        love.graphics.setColor(1,1,1,math.abs(brightness/20))
+    end
+    --love.graphics.setColor(0.5+brightness/10,0.5+brightness/10,0.5+brightness/10,math.abs(brightness/10))
+    love.graphics.rectangle("fill",0,0,1200,650)
 end
 
 function love.mousepressed(x, y, button, isTouch)
+    if hoveringExit then
+        os.exit()
+    elseif hoveringOptions then
+        scene="options-display"
+        hoveringOptions=false
+    elseif hoveringDisplay then
+        scene="options-display"
+    elseif hoveringMouse then
+        scene="options-mouse"
+    elseif hoveringGameplay then
+        scene="options-gameplay"
+    elseif hoveringFancyBackground then
+        useFancyBackground=not useFancyBackground
+    elseif hoveringBrightness then
+        if button==1 then
+            if brightness<5 then
+                brightness=brightness+0.5
+            end
+        elseif button==2 then
+            if brightness>-5 then
+                brightness=brightness-0.5
+            end
+        end
+    elseif hoveringBack then
+        scene="title"
+    end
     drawSelectionBox = true
     mouseOriginalX = x
     mouseOriginalY = y
